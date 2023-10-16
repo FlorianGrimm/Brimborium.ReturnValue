e9b3a00b0cf2a6067ba1d30385a21d493480c373
Florian Grimm <grimmuko@gmail.com>
2023-10-16 04:07:49 +0200

    step

--- Brimborium.ReturnValue.Documentation/Program.Tree.cs
+++ Brimborium.ReturnValue.Documentation/Program.Tree.cs
@@ -4,49 +4,53 @@ using Brimborium.ReturnValue;
 
 internal partial class Program
 {
-	static void MainTree()
-	{
-		var root = new Tree(null, "root");
-		var a = new Tree(root, string.Empty);
+    static void MainTree()
+    {
+        var root = new Tree(null, "root");
+        var a = new Tree(root, string.Empty);
 
-		#region assign Name but not Parent
-		
-		a = a with
-		{
-			Name = "a"
-		};
+        #region assign Name but not Parent
 
-		// - or -
+        a = a with
+        {
+            Name = "a"
+        };
 
-		a = a.WithNullable(null, "a");
+        // - or -
 
-		#endregion assign Name but not Parent
+        a = a.WithNullable(null, "a");
 
-		System.Diagnostics.Trace.Assert("root" == a.Parent?.Name);
-		System.Diagnostics.Trace.Assert("a" == a.Name);
+        #endregion assign Name but not Parent
+
+        System.Diagnostics.Trace.Assert("root" == a.Parent?.Name);
+        System.Diagnostics.Trace.Assert("a" == a.Name);
 
         var a2 = a.WithOptional(NoValue.Instance, "aa");
-		System.Diagnostics.Trace.Assert("aa" == a2.Name);
+        System.Diagnostics.Trace.Assert("aa" == a2.Name);
 
-		Tree? tree = null;
-		var a3 = a.WithOptional(tree.ToOptional(), "aa");
-		System.Diagnostics.Trace.Assert("aa" == a2.Name);
+        Tree? tree = null;
+        var a3 = a.WithOptional(tree.ToOptional(), "aa");
+        System.Diagnostics.Trace.Assert("aa" == a3.Name);
 
-	}
+        var a4 = a.WithOptional((Tree?)null, "aa");
+        System.Diagnostics.Trace.Assert("aa" == a4.Name);
+
+    }
 }
 
 #region sample Tree
 public record class Tree(
-	Tree? Parent,
-	string Name)
+    Tree? Parent,
+    string Name)
 {
-	public Tree WithNullable(Tree? parent, string? name) { 
-		return new Tree(parent??this.Parent, name??this.Name);
-	}
+    public Tree WithNullable(Tree? parent, string? name)
+    {
+        return new Tree(parent ?? this.Parent, name ?? this.Name);
+    }
 
-	public Tree WithOptional(Brimborium.ReturnValue.Optional<Tree?> parent, Optional<string> name)
-	{
-		return new Tree(parent.GetValueOrDefault(this.Parent), name.GetValueOrDefault(this.Name));
-	}
+    public Tree WithOptional(Brimborium.ReturnValue.Optional<Tree?> parent, Optional<string> name)
+    {
+        return new Tree(parent.GetValueOrDefault(this.Parent), name.GetValueOrDefault(this.Name));
+    }
 }
 #endregion sample Tree
